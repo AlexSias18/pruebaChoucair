bd7eb6f57cd876c5f14b4b1a08103c86a176a372
Alex Fernando Sias Shaw <alexshaw183@gmail.com>
2024-05-27 00:00:37 -0500

    Caso web

--- src/test/java/org/prueba/page/InicioPage.java
+++ src/test/java/org/prueba/page/InicioPage.java
@@ -10,11 +10,13 @@ public class InicioPage extends PageObject {
 
     @FindBy(xpath = "//*[@id=\"__next\"]/header/section/div/div[1]/button/div")
     public WebElementFacade burgerMenuBtn;
-    @FindBy(xpath = "//*[@id=\"__next\"]/header/aside/ul/section[2]/div/li[1]/div/div/p")
+    @FindBy(xpath = "//*[@id=\"__next\"]/header/aside/ul/section[2]/div/li[3]/div/div")
     public WebElementFacade categoryBtn;
-    @FindBy(xpath = "//*[@id=\"column-3\"]/li[1]/ul/li[4]/a")
+    @FindBy(xpath = "//*[@id=\"column-1\"]/li[1]/ul/li[1]/a")
     public WebElementFacade subCategoryBtn;
     @FindBy(xpath = "//*[@id=\"__next\"]/header/section/div/div[2]/div[2]/button")
     public  WebElementFacade carritoBtn;
 
+    @FindBy(xpath = "//*[@id=\"wps-overlay-close-button\"]")
+    public  WebElementFacade cerrarModal;
 }
--- src/test/java/org/prueba/steps/InicioSteps.java
+++ src/test/java/org/prueba/steps/InicioSteps.java
@@ -17,9 +17,11 @@ import org.prueba.utils.Utils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import java.util.HashSet;
 import java.util.Random;
 
 import java.time.Duration;
+import java.util.Set;
 
 import static org.prueba.utils.Utils.*;
 
@@ -52,26 +54,43 @@ public class InicioSteps {
     @Step("Elegir productos")
     public void elegirProductos() throws InterruptedException {
         Random random = new Random();
-        for (int i = 1; i <=1; i++) {
-            int randomIntInRange = random.nextInt(10)+1;
-            int randomCantidad = random.nextInt(9)+1;
+        Set<Integer> numerosSet = new HashSet<>();
+        while (numerosSet.size() < 3) {
+            int numero = random.nextInt(8) + 1;
+            if (numero != 2) {
+                numerosSet.add(numero);
+            }
+        }
+        Integer[] numerosArray = numerosSet.toArray(new Integer[0]);
+        for (int i = 0; i <numerosArray.length; i++) {
+            int randomCantidad = random.nextInt(3)+1;
             implicitlyWait(5);
-            WebElementFacade producto = inicioPage.findBy("//*[@id=\"__next\"]/main/section[5]/div/div[2]/div[2]/div[2]/ul/li["+randomIntInRange+"]/article/section/div[2]/div[2]/button");
-            WebElementFacade nombre = inicioPage.findBy("//*[@id=\"__next\"]/main/section[5]/div/div[2]/div[2]/div[2]/ul/li["+randomIntInRange+"]/article/section/div[1]/div[1]/div/h3/a");
+            WebElementFacade producto = inicioPage.findBy("//*[@id='__next']/main/section[3]/div/div[2]/div[2]/div[2]/ul/li["+numerosArray[i]+"]/article/section/div[2]/div[2]/button");
+            WebElementFacade nombre = inicioPage.findBy("//*[@id='__next']/main/section[3]/div/div[2]/div[2]/div[2]/ul/li["+numerosArray[i]+"]/article/section/div[1]/div/h3/a");
+            if (inicioPage.cerrarModal.isCurrentlyVisible()){
+                implicitlyWait(3);
+                inicioPage.cerrarModal.click();
+            }
             if (producto.isCurrentlyVisible()){
-                utils.scrollToElement(producto);
+                if (inicioPage.cerrarModal.isCurrentlyVisible()){
+                    implicitlyWait(3);
+                    inicioPage.cerrarModal.click();
+                }
                 Utils.saveVariableOnSession("nombre"+i,nombre.getText());
                 producto.waitUntilVisible();
                 producto.waitUntilClickable();
-                js.executeScript(script,producto);
+                producto.click();
             }
             implicitlyWait(5);
             for (int j = 0; j < randomCantidad; j++) {
-                WebElementFacade cantidad2=  inicioPage.findBy("//*[@id=\"__next\"]/main/section[5]/div/div[2]/div[2]/div[2]/ul/li["+randomIntInRange+"]/article/section/div[2]/div[2]/div/button[2]");
-                utils.scrollToElement(cantidad2);
+                WebElementFacade cantidad2=  inicioPage.findBy("//*[@id='__next']/main/section[3]/div/div[2]/div[2]/div[2]/ul/li["+numerosArray[i]+"]/article/section/div[2]/div[2]/div/button[2]");
+                if (inicioPage.cerrarModal.isCurrentlyVisible()){
+                    implicitlyWait(3);
+                    inicioPage.cerrarModal.click();
+                }
                 cantidad2.waitUntilVisible();
                 cantidad2.waitUntilClickable();
-                js.executeScript(script,cantidad2);
+                cantidad2.click();
                 implicitlyWait(5);
             }
             implicitlyWait(5);
@@ -80,17 +99,21 @@ public class InicioSteps {
         implicitlyWait(10);
     }
 
+    @Step("Validar productos")
     public void validarProductos() throws InterruptedException {
         implicitlyWait(3);
-        for (int i = 1; i <= 1; i++) {
-            String producto1= getVariableOnSession("nombre"+i);
+        for (int i = 1; i <= 3; i++) {
+            int variable= i-1;
+            String producto1= getVariableOnSession("nombre"+variable);
             implicitlyWait(3);
             WebElementFacade nombre = inicioPage.findBy("//*[@id=\"render-store.custom.checkout-io\"]/div/div[1]/div/div[1]/div/div/div/div[2]/div[3]/div[1]/div/div[2]/div["+i+"]/div/div[4]/span");
+            WebElementFacade productosCantidad = inicioPage.findBy("//*[@id=\"render-store.custom.checkout-io\"]/div/div[1]/div/div[1]/div/div/div/div[2]/div[3]/div[1]/div/div[3]");
             String productocarrito= nombre.getText();
             implicitlyWait(3);
+            Assert.assertEquals("3",productosCantidad.getText().substring(0,1));
             Assert.assertEquals(producto1,productocarrito);
         }
-
     }
+
 }
 
